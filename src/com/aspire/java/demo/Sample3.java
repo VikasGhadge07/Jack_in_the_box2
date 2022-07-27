c82939a6efc168cc8750d544de19d07b638802b1
vicky <vicky@LAPTOP-VJFLMR3J>
2022-07-27 21:57:56 +0530

    3rd commit

--- src/com/aspire/java/demo/Sample3.java
+++ src/com/aspire/java/demo/Sample3.java
@@ -1,5 +1,12 @@
 package com.aspire.java.demo;
 
-public class Sample3 {
+public class Sample3 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi..........");
+		
+		
+	}
 
 }
